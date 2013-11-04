7108cc256e01219f094c0992580c7307ced22d94
scottlow <slow@uvic.ca>
2013-11-03 23:21:26 -0800

    Modified DR proof comments

--- web2py/applications/cqg/question/caesar.py
+++ web2py/applications/cqg/question/caesar.py
@@ -32,25 +32,25 @@ class caesar:
 		html += "<p>Use a <b>caesar</b> cipher with key %d to encrypt the plain text.</p><center>" % self.key
 		plaintext_list = [("plain text", "right")]
 		for i in range(0, len(self.plaintext)):
-			plaintext_list.append(("<tt>" + self.plaintext[i] + "</tt>", "left_border center")) #DR1
+			plaintext_list.append(("<tt>" + self.plaintext[i] + "</tt>", "left_border center")) # DR1
 		ciphertext_list = [("cipher text", "top_border right")]
 		for i in range(0, len(self.ciphertext)):
 			if(i in self.hotspots):
-				ciphertext_list.append(("<tt>" + html_util.get_text("char_%i" % j, '' if answer['char_%i' % j] is None else answer['char_%i' % j]) + "</tt>", "left_border top_border"))
+				ciphertext_list.append(("<tt>" + html_util.get_text("char_%i" % j, '' if answer['char_%i' % j] is None else answer['char_%i' % j]) + "</tt>", "left_border top_border")) # DR2, DR3, DR4
 				j += 1
 			else:
-				ciphertext_list.append(("<tt>" + self.ciphertext[i] + "</tt>", "left_border top_border"))
+				ciphertext_list.append(("<tt>" + self.ciphertext[i] + "</tt>", "left_border top_border")) # DR 5
 		html += html_util.get_table([plaintext_list, ciphertext_list], "cellspacing=0 cellpadding=3")
 		html += "</center>"	
 
 		return html	
 
 	def get_input_element_ids(self):
-		return ['char_%i' % i for i in range(0, len(self.hotspots))]
+		return ['char_%i' % i for i in range(0, len(self.hotspots))] # DR6
 	
 	def check_answer(self,answer):
 		for i in range(0, len(self.hotspots)):
-			if(caesar_util.caeser_encrypt(self.plaintext[self.hotspots[i]], self.key) == answer['char_%i' % i]):
+			if(caesar_util.caeser_encrypt(self.plaintext[self.hotspots[i]], self.key) == answer['char_%i' % i]): # DR7, DR8
 				continue
 			else:
 				return False
